96cb915c704439f09692a5a5ecbca23f82415388
Andrew Moore <andrew@lessthanseventy.com>
2023-08-20 14:59:51 -0600

    disable block.nvim for filetypes

--- lua/plugins/ui/block.lua
+++ lua/plugins/ui/block.lua
@@ -2,12 +2,26 @@ return {
   {
     "HampusHauffman/block.nvim",
     init = function()
+      local keymap = vim.keymap.set
+      keymap("n", "<leader>Tb", "<cmd>Block<cr>", { desc = "Block Highlights" })
+
       vim.api.nvim_create_autocmd({ "FileType" }, {
         pattern = { "*" },
-        callback = function()
+        callback = function(opts)
           local buf_id = vim.api.nvim_get_current_buf()
           local ft = vim.bo[buf_id].filetype
-          if ft == "toggleterm" or ft == "Trouble" or ft == "iron" or ft == "fzf" or ft == "oil" then
+          local buftype = vim.bo[buf_id].buftype
+          if buftype == "nofile" then
+            return
+          end
+          if
+            ft == "toggleterm"
+            or ft == "Trouble"
+            or ft == "iron"
+            or ft == "fzf"
+            or ft == "oil"
+            or ft == "NvimSeparator"
+          then
             return
           else
             vim.cmd("BlockOn")
